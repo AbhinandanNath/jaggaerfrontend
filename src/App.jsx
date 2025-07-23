d7f189dfa99a126c815b501000b7af76dc72a39b
AbhinandanNath <abhi.nath120@gmail.com>
2025-07-23 19:18:34 +0530

    Responsive changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import "./App.css";
-import Header from "./Header/Header";
 import ProductPage from "./products/ProductPage";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <ProductPage />
-    </>
-  );
+  return <ProductPage />;
 }
 
 export default App;
